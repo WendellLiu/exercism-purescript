767eba02a73483ab219d226ed9dd32f1fe2e083a
WendellLiu <cuk.bas@gmail.com>
2018-03-07 18:17:16 +0800

    zipTwoString

--- hamming/src/Hamming.purs
+++ hamming/src/Hamming.purs
@@ -4,13 +4,19 @@ module Hamming
   ) where
 
 import Prelude
-import Data.String (length)
+import Data.String (length, toCharArray)
 import Data.Maybe (Maybe(Just, Nothing))
+import Data.Array (zip)
+import Data.Tuple (Tuple(..))
 
 isSameLong :: String -> String -> Boolean
 isSameLong str1 = ((==) (length str1)) <<< length
 
+zipTwoString :: String -> String -> Array (Tuple Char Char)
+zipTwoString dna1 dna2 = zip (toCharArray dna1) (toCharArray dna2)
+
+
 distance :: String -> String -> Maybe Int
 distance dna1 dna2 = case isSameLong dna1 dna2 of
   false -> Nothing
-  _ -> Just 1
+  true -> Just 1
