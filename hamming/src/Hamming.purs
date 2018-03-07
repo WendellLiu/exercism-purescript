309c2b7ab70a6147817b8e4e53db6744a4284238
WendellLiu <cuk.bas@gmail.com>
2018-03-07 17:09:17 +0800

    isSameLong

--- hamming/src/Hamming.purs
+++ hamming/src/Hamming.purs
@@ -1,3 +1,16 @@
 module Hamming
-  ( distance
+  ( distance,
+    isSameLong
   ) where
+
+import Prelude
+import Data.String (length)
+import Data.Maybe (Maybe(Just, Nothing))
+
+isSameLong :: String -> String -> Boolean
+isSameLong str1 = ((==) (length str1)) <<< length
+
+distance :: String -> String -> Maybe Int
+distance dna1 dna2 = case isSameLong dna1 dna2 of
+  false -> Nothing
+  _ -> Just 1
