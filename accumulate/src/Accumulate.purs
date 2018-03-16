25451c0d50c64a270c11bcf77c28a7626c7d2f9f
Wendell Liu <cuk.bas@gmail.com>
2018-03-16 17:57:55 +0800

    Accumulate prototype

--- accumulate/src/Accumulate.purs
+++ accumulate/src/Accumulate.purs
@@ -1,3 +1,13 @@
 module Accumulate
   ( accumulate
   ) where
+
+import Prelude
+import Data.List.Types (List)
+import Data.List (concat, null, head, tail, singleton)
+
+
+accumulate :: forall a. (a -> a) -> List a -> List a
+accumulate acc f = case null f of
+  false -> concat (singleton acc (head f)) $ accumulate acc (tail f)
+  true -> acc f
