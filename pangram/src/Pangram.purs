f2872102b74a88dc59751e96a9cdd320e251e998
WendellLiu <cuk.bas@gmail.com>
2018-03-16 11:27:41 +0800

    finish pangram

--- pangram/src/Pangram.purs
+++ pangram/src/Pangram.purs
@@ -1,3 +1,18 @@
 module Pangram
   ( isPangram
   ) where
+
+import Prelude
+import Data.Either (Either(Right))
+import Data.String (toLower)
+import Data.String.Regex (Regex, regex, test)
+import Data.String.Regex.Flags (noFlags)
+import Partial.Unsafe (unsafePartial)
+
+testRe :: Regex
+testRe = unsafePartial case regex "(?=.*a)(?=.*b)(?=.*c)(?=.*d)(?=.*e)(?=.*f)(?=.*g)(?=.*h)(?=.*i)(?=.*j)(?=.*k)(?=.*l)(?=.*m)(?=.*n)(?=.*o)(?=.*p)(?=.*q)(?=.*r)(?=.*s)(?=.*t)(?=.*u)(?=.*v)(?=.*w)(?=.*x)(?=.*y)(?=.*z)." noFlags of
+                Right r -> r
+
+
+isPangram :: String -> Boolean
+isPangram = (test testRe <<< toLower)
